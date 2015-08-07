bd891077988ee4eb06e39dcb4760f3aead97df31
Ipsis <ipsis418@gmail.com>
2015-08-07 19:25:17 +0100

    Use enum for defined messages

--- src/main/java/ipsis/wini/network/PacketHandler.java
+++ src/main/java/ipsis/wini/network/PacketHandler.java
@@ -10,13 +10,30 @@ public class PacketHandler {
 
     public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(Reference.MOD_ID.toLowerCase());
 
+    private enum ModMessage {
+        GUI_WIDGET(MessageGuiWidget.class, Side.SERVER),
+        GUI_FIXED_PROGRESS_BASE(MessageGuiFixedProgressBar.class, Side.CLIENT),
+        TE_DESCRIPTION(MessageTileEntityWini.class, Side.CLIENT),
+        HYST_CFG(MessageHysteresisCfg.class, Side.SERVER),
+        RS_OUTPUT_CFG(MessageRedstoneOutputCfg.class, Side.SERVER),
+        HYST_UPDATE(MessageHysteresisUpdate.class, Side.CLIENT);
+
+        private Class clazz;
+        private Side side;
+        ModMessage(Class clazz, Side side) {
+            this.clazz = clazz;
+            this.side = side;
+        }
+
+        public Class getClazz() { return this.clazz; }
+        public Side getSide() { return this.side; }
+
+    }
+
     public static void init() {
 
-        INSTANCE.registerMessage(MessageGuiWidget.class, MessageGuiWidget.class, 0, Side.SERVER);
-        INSTANCE.registerMessage(MessageGuiFixedProgressBar.class, MessageGuiFixedProgressBar.class, 1, Side.CLIENT);
-        INSTANCE.registerMessage(MessageTileEntityWini.class, MessageTileEntityWini.class, 2, Side.CLIENT);
-        INSTANCE.registerMessage(MessageHysteresisCfg.class, MessageHysteresisCfg.class, 3, Side.SERVER);
-        INSTANCE.registerMessage(MessageRedstoneOutputCfg.class, MessageRedstoneOutputCfg.class, 4, Side.SERVER);
-        INSTANCE.registerMessage(MessageHysteresisUpdate.class, MessageHysteresisUpdate.class, 5, Side.CLIENT);
+        for (ModMessage m : ModMessage.values())
+            INSTANCE.registerMessage(m.getClazz(), m.getClazz(), m.ordinal(), m.getSide());
     }
+
 }
