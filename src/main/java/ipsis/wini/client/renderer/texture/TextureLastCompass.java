aa7a0a77a4de966470f53119afbd09d200e67caa
Ipsis <ipsis418@gmail.com>
2015-08-07 12:47:15 +0100

    LastCompass now points to stronghold

--- src/main/java/ipsis/wini/client/renderer/texture/TextureLastCompass.java
+++ src/main/java/ipsis/wini/client/renderer/texture/TextureLastCompass.java
@@ -1,16 +1,14 @@
 package ipsis.wini.client.renderer.texture;
 
+import cofh.lib.util.position.BlockPosition;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import ipsis.oss.util.LogHelper;
 import ipsis.wini.reference.Names;
 import ipsis.wini.reference.Textures;
+import ipsis.wini.registry.LocationRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureCompass;
 import net.minecraft.client.renderer.texture.TextureUtil;
-import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.World;
 
 @SideOnly(Side.CLIENT)
@@ -28,14 +26,12 @@ public class TextureLastCompass extends TextureAtlasSprite {
 
         if (minecraft.theWorld != null && minecraft.thePlayer != null) {
 
-            ChunkPosition p = minecraft.theWorld.findClosestStructure("Stronghold",
-                    (int)minecraft.thePlayer.posX, (int)minecraft.thePlayer.posY, (int)minecraft.thePlayer.posZ);
-
-            LogHelper.info(p);
+            BlockPosition p = LocationRegistry.getInstance().getCurrentLocation();
             if (p != null)
-                this.updateCompass(minecraft.theWorld, minecraft.thePlayer.posX, minecraft.thePlayer.posZ, (double)minecraft.thePlayer.rotationYaw, false, false);
+                this.updateCompass(minecraft.theWorld,
+                        minecraft.thePlayer.posX, minecraft.thePlayer.posZ, (double) minecraft.thePlayer.rotationYaw);
             else
-            super.updateAnimation();
+                super.updateAnimation();
         } else {
             super.updateAnimation();
         }
@@ -43,74 +39,57 @@ public class TextureLastCompass extends TextureAtlasSprite {
 
     /**
      * Updates the compass based on the given x,z coords and camera direction
-     * Exactly the same as the default compass EXCEPT it looks for a stronghold!
+     * Exactly the same as the default compass EXCEPT it looks for a requested location
+     *
+     * I dont understand any of this :)
+     * It is working out which frame of the texture to display.
      */
-    public void updateCompass(World world, double posX, double posZ, double p_94241_6_, boolean p_94241_8_, boolean p_94241_9_)
-    {
-        //if (!this.framesTextureData.isEmpty())
-        {
+    public void updateCompass(World world, double posX, double posZ, double rotationYaw) {
+        if (!this.framesTextureData.isEmpty()) {
+
             double d3 = 0.0D;
-            LogHelper.info("updateCompass: " + posX + " " + posZ + " " + p_94241_6_ + " " + p_94241_8_ + " " + p_94241_9_);
 
-            ChunkPosition chunkposition;
-            if (world != null && !p_94241_8_ && (chunkposition = world.findClosestStructure("Stronghold", (int)posX, 32, (int)posZ)) != null)
-            {
-                LogHelper.info("updateCompass " + world + " " + chunkposition.chunkPosX + " " + chunkposition.chunkPosZ);
-                double d4 = (double)chunkposition.chunkPosX - posX;
-                double d5 = (double)chunkposition.chunkPosZ - posZ;
-                p_94241_6_ %= 360.0D;
-                d3 = -((p_94241_6_ - 90.0D) * Math.PI / 180.0D - Math.atan2(d5, d4));
+            BlockPosition p = LocationRegistry.getInstance().getCurrentLocation();
+            if (world != null && p != null) {
+                double d4 = (double) p.x - posX;
+                double d5 = (double) p.z - posZ;
+                rotationYaw %= 360.0D;
+                d3 = -((rotationYaw - 90.0D) * Math.PI / 180.0D - Math.atan2(d5, d4));
 
+                /* TODO if not overworld - do I want this? */
                 if (!world.provider.isSurfaceWorld())
-                {
                     d3 = Math.random() * Math.PI * 2.0D;
-                }
             }
 
-            if (p_94241_9_)
-            {
-                this.currentAngle = d3;
-            }
-            else
-            {
-                double d6;
+            double d6;
 
-                for (d6 = d3 - this.currentAngle; d6 < -Math.PI; d6 += (Math.PI * 2D))
-                {
-                    ;
-                }
-
-                while (d6 >= Math.PI)
-                {
-                    d6 -= (Math.PI * 2D);
-                }
-
-                if (d6 < -1.0D)
-                {
-                    d6 = -1.0D;
-                }
-
-                if (d6 > 1.0D)
-                {
-                    d6 = 1.0D;
-                }
-
-                this.angleDelta += d6 * 0.1D;
-                this.angleDelta *= 0.8D;
-                this.currentAngle += this.angleDelta;
-            }
-
-            int i;
-
-            for (i = (int)((this.currentAngle / (Math.PI * 2D) + 1.0D) * (double)this.framesTextureData.size()) % this.framesTextureData.size(); i < 0; i = (i + this.framesTextureData.size()) % this.framesTextureData.size())
-            {
+            for (d6 = d3 - this.currentAngle; d6 < -Math.PI; d6 += (Math.PI * 2D)) {
                 ;
             }
 
-            if (i != this.frameCounter)
-            {
+            while (d6 >= Math.PI)
+                d6 -= (Math.PI * 2D);
+
+            if (d6 < -1.0D)
+                d6 = -1.0D;
+
+            if (d6 > 1.0D)
+                d6 = 1.0D;
+
+            this.angleDelta += d6 * 0.1D;
+            this.angleDelta *= 0.8D;
+            this.currentAngle += this.angleDelta;
+
+            int i;
+            for (i = (int) ((this.currentAngle / (Math.PI * 2D) + 1.0D) * (double) this.framesTextureData.size()) % this.framesTextureData.size();
+                 i < 0;
+                 i = (i + this.framesTextureData.size()) % this.framesTextureData.size()) {
+                ;
+            }
+
+            if (i != this.frameCounter) {
                 this.frameCounter = i;
-                TextureUtil.uploadTextureMipmap((int[][])this.framesTextureData.get(this.frameCounter), this.width, this.height, this.originX, this.originY, false, false);
+                TextureUtil.uploadTextureMipmap((int[][]) this.framesTextureData.get(this.frameCounter), this.width, this.height, this.originX, this.originY, false, false);
             }
         }
     }
--- src/main/java/ipsis/wini/item/ItemLastCompass.java
+++ src/main/java/ipsis/wini/item/ItemLastCompass.java
@@ -2,10 +2,23 @@ package ipsis.wini.item;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import ipsis.oss.util.LogHelper;
 import ipsis.wini.handler.TextureEventHandlers;
+import ipsis.wini.network.PacketHandler;
+import ipsis.wini.network.message.MessageStrutureLocation;
 import ipsis.wini.reference.Names;
+import ipsis.wini.registry.LocationRegistry;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.ChunkPosition;
+import net.minecraft.world.World;
 
+/**
+ * Compass spins when it has no location
+ * Shift-rightclick to set to the nearest stronghold
+ */
 public class ItemLastCompass extends ItemWini {
 
     public ItemLastCompass() {
@@ -17,5 +30,18 @@ public class ItemLastCompass extends ItemWini {
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister iconRegister) {
         this.itemIcon = TextureEventHandlers.lastCompass;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
+
+        if (!world.isRemote && entityPlayer.isSneaking()) {
+            ChunkPosition chunkposition = world.findClosestStructure(LocationRegistry.StructureType.STRONGHOLD.name, (int) entityPlayer.posX, (int) entityPlayer.posY, (int) entityPlayer.posZ);
+            /* Forge Docs - as long as you are on the server you can safely cast any EntityPlayer to EntityPlayerMP */
+            if (chunkposition != null)
+                PacketHandler.INSTANCE.sendTo(new MessageStrutureLocation(chunkposition.chunkPosX, chunkposition.chunkPosY, chunkposition.chunkPosZ), (EntityPlayerMP)entityPlayer);
+        }
+
+        return itemStack;
     }
 }
