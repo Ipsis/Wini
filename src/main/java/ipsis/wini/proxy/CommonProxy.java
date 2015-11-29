d906943970d92b64cf5798d76f0f3c0c454e6c35
Ipsis <ipsis418@gmail.com>
2015-11-29 12:10:38 +0000

    Only register the TextureStitchEvent on the client side. Closes issue 2

--- src/main/java/ipsis/wini/proxy/CommonProxy.java
+++ src/main/java/ipsis/wini/proxy/CommonProxy.java
@@ -4,7 +4,6 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import ipsis.wini.handler.ItemEventHandler;
 import ipsis.wini.handler.LivingEventHandler;
 import ipsis.wini.handler.PlayerEventHandler;
-import ipsis.wini.handler.TextureEventHandlers;
 import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy implements  IProxy {
@@ -16,7 +15,6 @@ public class CommonProxy implements  IProxy {
          *  TODO LivingEventHandler registered twice!
          *  FMLCommonHandler.instance().bus().register for PlayerTickEvent == LivingEventHandler
          */
-        MinecraftForge.EVENT_BUS.register(new TextureEventHandlers());
         MinecraftForge.EVENT_BUS.register(new ItemEventHandler());
         MinecraftForge.EVENT_BUS.register(new LivingEventHandler());
         FMLCommonHandler.instance().bus().register(new PlayerEventHandler());
